9c4bfc65a1cc0815a94f83b4a0b7877a8e47450c
Hongtae Kim <tiff2766@gmail.com>
2015-06-05 15:32:04 +0900

    no message

--- DK/DKFoundation/DKSpinLock.cpp
+++ DK/DKFoundation/DKSpinLock.cpp
@@ -2,7 +2,7 @@
 //  File: DKSpinLock.h
 //  Author: Hongtae Kim (tiff2766@gmail.com)
 //
-//  Copyright (c) 2004-2014 Hongtae Kim. All rights reserved.
+//  Copyright (c) 2004-2015 Hongtae Kim. All rights reserved.
 //
 
 #include "DKSpinLock.h"
@@ -40,7 +40,7 @@ void DKSpinLock::Lock(void) const
 
 bool DKSpinLock::TryLock(void) const
 {
-	return state.CompareAndSet(SpinLockStateFree, Private::SpinLockStateLocked);
+	return state.CompareAndSet(SpinLockStateFree, SpinLockStateLocked);
 }
 
 void DKSpinLock::Unlock(void) const
